bf7e455808871c57840777602b82fe083280ccd2
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2016-07-25 10:15:50 +0200

    adding dropdown field

---
+++ code/BootstrapDropdownField.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Description of BootstrapDropdownField
+ *
+ * @author Patrick Chitovoro
+ * @copyright (c) 2016, Chito Systems
+ */
+class BootstrapDropdownField extends DropdownField
+{
+    protected $extraClasses = array('form-control');
+
+}
